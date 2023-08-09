1f47d61d9f07ca886e0f5770844d230ac4921c30
Dan McGrath <mcgarthd3254@yahoo.com>
2023-08-09 18:59:49 -0400

    add func that allow pages to change based on nav

--- src/nav.js
+++ src/nav.js
@@ -1,9 +1,13 @@
 import loadHomepage from "./homepage";
+import createContactPage from "./contact";
 
-const createNav = () => {
+const loadNav = () => {
     let content = document.querySelector('#content');
+    let header = document.createElement('header');
+    content.appendChild(header);
+
     let nav = document.createElement('nav');
-    content.appendChild(nav);
+    header.appendChild(nav);
 
     let navLinks = document.createElement('ul');
     nav.appendChild(navLinks)
@@ -11,20 +15,50 @@ const createNav = () => {
     let homeNav = document.createElement('li');
     homeNav.textContent = 'Home';
     homeNav.dataset.active = 'true';
-    
+    homeNav.classList.add('nav-link');
+    homeNav.setAttribute('id', 'home');
+
     let menuNav = document.createElement('li');
     menuNav.textContent = 'Menu';
-    menuNav.dataset.active = 'false'
+    menuNav.dataset.active = 'false';
+    menuNav.classList.add('nav-link');
+    menuNav.setAttribute('id', 'menu');
 
     let contactNav = document.createElement('li');
     contactNav.textContent = 'Contact';
     contactNav.dataset.active = 'false';
+    contactNav.classList.add('nav-link');
+    contactNav.setAttribute('id', 'contact')
 
     navLinks.appendChild(homeNav);
     navLinks.appendChild(menuNav);
     navLinks.appendChild(contactNav);
 
+    const navList = document.querySelectorAll('.nav-link');
+
+    // click event for active page
+    const activeLink = (e) => {
+        if (e.target.dataset === 'true') {
+            return
+        } else {
+            navList.forEach(ele => ele.dataset.active = 'false')
+            e.target.dataset.active = 'true';
+
+            while (content.childNodes.length > 1) {
+                content.removeChild(content.lastChild)
+            }
+            if (e.target.id === 'home') {
+                loadHomepage();
+            } else if (e.target.id === 'contact') {
+                createContactPage();
+            }
+        }
+    }
+
+    navList.forEach(ele => ele.addEventListener('click', activeLink))
 
 }
 
-export default createNav
+
+
+export default loadNav
