4e894c9c91fe200d8ab410e409d5854b36597002
Dan McGrath <mcgarthd3254@yahoo.com>
2023-08-09 19:10:46 -0400

    make menu function

---
+++ src/menu.js
@@ -0,0 +1,10 @@
+const loadMenu = () => {
+    const content = document.querySelector('#content');
+
+    const title = document.createElement('h1');
+    title.textContent = 'Menu';
+
+    content.appendChild(title);
+}
+
+export default loadMenu
